54fe4b5616769d65bdb8a4338a5fa5baaacfb59d
17matb <147427088+17matb@users.noreply.github.com>
2025-07-09 16:16:11 +0200

    Ajout des tables book et review avec les clés étrangères appropriées dans create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -13,6 +13,15 @@ CREATE TABLE author (
 );
 
 CREATE TABLE book (
+    book_id INT PRIMARY KEY,
+    author_id INT NOT NULL,
+    theme_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    publication_date DATE,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES author(author_id),
+    FOREIGN KEY (theme_id) REFERENCES theme(theme_id)
 );
 
 CREATE TABLE customer (
@@ -32,4 +41,11 @@ CREATE TABLE customer_order (
 );
 
 CREATE TABLE review (
+    customer_id INT NOT NULL,
+    book_id INT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment VARCHAR,
+    review_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (book_id) REFERENCES book(book_id)
 );
