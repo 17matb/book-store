66633b86e3a03ef0cc6b17fb59e9f29fb6b0ca48
17matb <147427088+17matb@users.noreply.github.com>
2025-07-10 11:23:10 +0200

    Amélioration de la lisibilité des requêtes SQL dans 1_request_exercice.sql et 2_request_exercice.sql en ajoutant des sauts de ligne pour une meilleure structure.

--- 2_request_exercice.sql
+++ 2_request_exercice.sql
@@ -1,14 +1,25 @@
 -- 1. Affichez la somme totale des stocks de livres.
-SELECT SUM(stock) AS 'Somme totale des stocks de livres' FROM book;
+SELECT SUM(stock) AS 'Somme totale des stocks de livres' 
+FROM book;
 
 -- 2. Affichez pour chaque auteur le nombre de livres écrits.
-SELECT a.first_name, a.last_name, COUNT(b.book_id) FROM author a JOIN book b ON a.author_id = b.author_id GROUP BY a.author_id;
+SELECT a.first_name, a.last_name, COUNT(b.book_id) 
+FROM author a 
+JOIN book b ON a.author_id = b.author_id 
+GROUP BY a.author_id;
 
 -- 3. Affichez le titre du livre, nom complet de l’auteur, et nom du thème.
-SELECT b.title, a.first_name, a.last_name, t.theme_name FROM book b JOIN author a ON b.author_id = a.author_id JOIN theme t ON b.theme_id = t.theme_id;
+SELECT b.title, a.first_name, a.last_name, t.theme_name 
+FROM book b 
+JOIN author a ON b.author_id = a.author_id 
+JOIN theme t ON b.theme_id = t.theme_id;
 
 -- 4. Affichez toutes les commandes d’un client donné avec titre du livre et date de commande.
-SELECT b.title, co.order_date FROM book b JOIN customer_order co ON b.book_id = co.book_id JOIN customer c ON co.customer_id = c.customer_id WHERE c.customer_id = 1; /* si l'on veut afficher les commandes d'un client donné, on cherche son customer_id */
+SELECT b.title, co.order_date 
+FROM book b 
+JOIN customer_order co ON b.book_id = co.book_id 
+JOIN customer c ON co.customer_id = c.customer_id 
+WHERE c.customer_id = 1; /* si l'on veut afficher les commandes d'un client donné, on cherche son customer_id */
 
 -- 5. Affichez la moyenne des notes pour chaque livre ayant au moins 3 avis.
 SELECT b.title, AVG(r.rating)
